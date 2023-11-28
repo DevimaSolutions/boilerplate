15e903164192df7e9514eaef869d08c17d584925
Viktoria Monastyrska <viktoria.m@devima.solutions>
2023-11-28 14:30:08 +0200

    feat: sign in page styles

--- apps/frontend/src/components/auth/SignInForm/SignInForm.tsx
+++ apps/frontend/src/components/auth/SignInForm/SignInForm.tsx
@@ -13,10 +13,39 @@ export function SignInForm({ onSubmit }: SignInFormProps) {
   return (
     <Formik initialValues={initialValues} onSubmit={onSubmit}>
       {({ isSubmitting }) => (
-        <Form>
-          <Field name="email" placeholder="email" type="email" />
-          <Field name="password" placeholder="password" type="password" />
-          <button disabled={isSubmitting} type="submit">
+        <Form className="space-y-6">
+          <div>
+            <label className="block text-sm font-medium leading-6 text-gray-900" htmlFor="email">
+              Email address
+            </label>
+            <div className="mt-2">
+              <Field
+                className="block box-border	 w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                label="Email address"
+                name="email"
+                placeholder="email"
+                type="email"
+              />
+            </div>
+          </div>
+          <div>
+            <label className="block text-sm font-medium leading-6 text-gray-900" htmlFor="email">
+              Email address
+            </label>
+            <div className="mt-2">
+              <Field
+                className="block box-border	 w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                name="password"
+                placeholder="password"
+                type="password"
+              />
+            </div>
+          </div>
+          <button
+            className="flex w-full justify-center btn btn-primary"
+            disabled={isSubmitting}
+            type="submit"
+          >
             Sign in
           </button>
         </Form>
