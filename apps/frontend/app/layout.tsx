d919ef4400f9fb60e77a59ace3243ad9923e39f1
Viktoria Monastyrska <viktoria.m@devima.solutions>
2023-11-29 14:05:54 +0200

    fix: redundant and conditional styles

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
-      <body className={`m-0 ${inter.className}`}>
+      <body className={inter.className}>
         <Header />
         {children}
         <ToastContainer />
--- apps/frontend/src/components/header/Header.tsx
+++ apps/frontend/src/components/header/Header.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -8,7 +9,12 @@ import { SignOutButton } from '../auth/SignOutButton';
 export default async function Header() {
   const user = await getServerUser();
   return (
-    <header className="static top-0 w-full h-[60px] px-[40px] flex items-center justify-between bg-primary shadow-[0_0px_16px_0px_rgba(22,74,162,0.06)]">
+    <header
+      className={clsx(
+        'top-0 w-full h-[60px] px-[40px] flex items-center bg-primary shadow-[0_0px_16px_0px_rgba(22,74,162,0.06)]',
+        user ? 'justify-between' : 'justify-end',
+      )}
+    >
       {user ? (
         <>
           <Link className="text-white flex items-center gap-[10px]" href="/profile">
@@ -22,15 +28,13 @@ export default async function Header() {
           <SignOutButton />
         </>
       ) : (
-        <div className="w-full flex justify-end">
-          <Link
-            className="btn btn-primary justify-self-end text-white border-none"
-            href="/sign-in"
-            type="button"
-          >
-            Sign In
-          </Link>
-        </div>
+        <Link
+          className="btn btn-primary justify-self-end text-white border-none"
+          href="/sign-in"
+          type="button"
+        >
+          Sign In
+        </Link>
       )}
     </header>
   );
