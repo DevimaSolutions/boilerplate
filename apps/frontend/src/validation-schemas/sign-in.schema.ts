b39e18a2005f9510dc628f0b76fea376464887d5
Viktoria Monastyrska <viktoria.m@devima.solutions>
2023-11-29 12:05:04 +0200

    feat: added zod validation and error handling

---
+++ apps/frontend/src/validation-schemas/sign-in.schema.ts
@@ -0,0 +1,6 @@
+import { z } from 'zod';
+
+export const signInSchema = z.object({
+  email: z.string().trim().toLowerCase().email().min(1).max(255),
+  password: z.string().trim().min(8).max(255),
+});
