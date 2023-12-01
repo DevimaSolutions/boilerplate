c8dd9f7733106cca2095b6cf381c93478fc82a6e
Viktoria Monastyrska <viktoria.m@devima.solutions>
2023-12-01 16:45:35 +0200

    fix: password validation

--- apps/backend/src/features/auth/validations/reset-password.schema.ts
+++ apps/backend/src/features/auth/validations/reset-password.schema.ts
@@ -2,5 +2,5 @@ import { z } from 'zod';
 
 export const resetPasswordSchema = z.object({
   token: z.string().trim().min(1),
-  password: z.string().trim().min(8).max(255),
+  password: z.string().min(8).max(255),
 });
