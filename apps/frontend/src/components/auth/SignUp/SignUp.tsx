d744541c28bee05ce585e6d4c4f557215cc3f9b0
Viktoria Monastyrska <viktoria.m@devima.solutions>
2023-12-01 15:44:48 +0200

    feat: sign up page and base logic

--- apps/frontend/src/components/auth/SignUp/SignUp.tsx
+++ apps/frontend/src/components/auth/SignUp/SignUp.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import Link from 'next/link';
+
 import { SignUpForm } from '../SignUpForm';
 
 import { useSignUp } from './useSignUp';
@@ -7,9 +9,17 @@ import { useSignUp } from './useSignUp';
 export function SignUp() {
   const { onSubmit } = useSignUp();
   return (
-    <main>
-      <h1>Sign Up</h1>
+    <main className="h-full flex flex-col justify-center mt-10 sm:mx-auto sm:w-full sm:max-w-sm px-6 py-12 ">
+      <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+        Sign up
+      </h2>
       <SignUpForm onSubmit={onSubmit} />
+      <p className="flex w-full justify-center gap-2">
+        Already have an account?
+        <Link className="link-primary" href="/sign-in">
+          Sign in
+        </Link>
+      </p>
     </main>
   );
 }
---
+++ apps/frontend/src/components/auth/SignUpForm/schema.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+
+export const signUpSchema = z
+  .object({
+    email: z.string().trim().toLowerCase().email().min(1).max(255),
+    password: z.string().trim().min(8).max(255),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ['confirmPassword'],
+  });
