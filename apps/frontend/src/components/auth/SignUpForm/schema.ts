0ad361f639d5baf0bfc4d9e40c92479dfc8063ef
Viktoria Monastyrska <viktoria.m@devima.solutions>
2023-12-01 17:22:19 +0200

    fix: password validation

--- apps/frontend/src/components/auth/SignUpForm/schema.ts
+++ apps/frontend/src/components/auth/SignUpForm/schema.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 export const signUpSchema = z
   .object({
     email: z.string().trim().toLowerCase().email().min(1).max(255),
-    password: z.string().trim().min(8).max(255),
+    password: z.string().min(8).max(255),
     confirmPassword: z.string(),
   })
   .refine((data) => data.password === data.confirmPassword, {
