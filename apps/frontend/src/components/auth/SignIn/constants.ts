2ea735157cf8a5bb2ea250f96455645a475078c0
Viktoria Monastyrska <viktoria.m@devima.solutions>
2023-12-01 15:19:15 +0200

    fix: naming, redundant code

--- apps/frontend/src/components/auth/SignIn/constants.ts
+++ apps/frontend/src/components/auth/SignIn/constants.ts
@@ -1,3 +1,3 @@
 export const signInErrorMessagesMap: Record<string, string> = {
-  AccessDenied: "You don't have permissions to sign in",
+  accessDenied: "You don't have permissions to sign in",
 };
