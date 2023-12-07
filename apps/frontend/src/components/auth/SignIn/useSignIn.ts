8a1f20b27064a4a90f609aa629ad70d658237816
viktoria-devima-solutions <viktoria.m@devima.solutions>
2023-12-07 21:19:46 +0200

    fix: replaced router with callbackUrl

--- apps/frontend/src/components/auth/SignIn/useSignIn.ts
+++ apps/frontend/src/components/auth/SignIn/useSignIn.ts
@@ -1,4 +1,4 @@
-import { useRouter, useSearchParams } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
 import { signIn } from 'next-auth/react';
 import { useEffect } from 'react';
 import { toast } from 'react-toastify';
@@ -9,8 +9,6 @@ import type { SignInFormValues } from '../SignInForm';
 import type { FormikHelpers } from 'formik';
 
 export const useSignIn = () => {
-  const router = useRouter();
-
   const searchParams = useSearchParams();
   const signInError = searchParams.get('error');
   const redirectRoute = searchParams.get('route');
@@ -29,7 +27,7 @@ export const useSignIn = () => {
     // Call NextAuth api route
     const response = await signIn('credentials', {
       ...values,
-      redirect: false,
+      callbackUrl: redirectRoute ?? '/',
     });
     if (!response?.ok) {
       setErrors({
@@ -38,11 +36,6 @@ export const useSignIn = () => {
           'We couldn`t find an account matching the email and password you entered. Please check your email and password and try again',
       });
     }
-    if (redirectRoute?.length) {
-      router.replace(redirectRoute);
-      return;
-    }
-    router.refresh();
   };
 
   const onGoogleSignIn = async () => {
