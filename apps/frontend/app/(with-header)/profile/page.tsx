9f8f50a0d0a76c10afa4b1219b0200f81745d01b
Viktoria Monastyrska <viktoria.m@devima.solutions>
2023-12-08 13:47:24 +0200

    fix: redundant props passing

--- apps/frontend/app/(with-header)/profile/page.tsx
+++ apps/frontend/app/(with-header)/profile/page.tsx
@@ -1,14 +1,9 @@
-import { headers } from 'next/headers';
-
 import { Profile } from 'src/components/auth/Profile';
 import { requireAuthorizedUser } from 'src/utils/auth.util';
 
 export default async function ProfilePage() {
-  const headersList = headers();
-  const pathname = headersList.get('x-pathname');
-
   // TODO: use NextAuth App Router guide for caching
-  await requireAuthorizedUser({ currentRoute: pathname ?? '' });
+  await requireAuthorizedUser();
 
   return <Profile />;
 }
