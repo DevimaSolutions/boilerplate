90679f4981a9774161349080f3c11db36e7db462
Viktoria Monastyrska <viktoria.m@devima.solutions>
2023-12-08 11:31:05 +0200

    fix: redundant refresh in sign out

--- apps/frontend/src/components/auth/SignOutButton/SignOutButton.tsx
+++ apps/frontend/src/components/auth/SignOutButton/SignOutButton.tsx
@@ -1,14 +1,10 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
 import { signOut } from 'next-auth/react';
 
 export function SignOutButton() {
-  const router = useRouter();
-
   const onSignOut = async () => {
-    await signOut({ redirect: false });
-    router.refresh();
+    await signOut();
   };
 
   return (
