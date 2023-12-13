3a2a76d4ee5c7323ab3bbf3f0f290234bbb8f2cf
Viktoria Monastyrska <viktoria.m@devima.solutions>
2023-12-13 16:35:31 +0200

    feat: edit button for avatar

--- apps/frontend/src/components/Avatar/Avatar.tsx
+++ apps/frontend/src/components/Avatar/Avatar.tsx
@@ -1,18 +1,30 @@
-import { UserCircleIcon } from '@heroicons/react/24/outline';
+import { PencilSquareIcon, UserCircleIcon } from '@heroicons/react/24/outline';
 import Image from 'next/image';
 
 import type { AvatarProps } from './types';
 
-export default function Avatar({ imageUri, size }: AvatarProps) {
-  return imageUri ? (
-    <Image
-      alt="Profile picture"
-      className="object-cover object-center rounded-full"
-      height={size}
-      src={imageUri}
-      width={size}
-    />
-  ) : (
-    <UserCircleIcon className={`h-[${size}] w-[${size}]`} />
+export default function Avatar({ imageUri, size, onEdit }: AvatarProps) {
+  return (
+    <div className="relative">
+      {imageUri ? (
+        <Image
+          alt="Profile picture"
+          className="object-cover object-center rounded-full"
+          height={size}
+          src={imageUri}
+          width={size}
+        />
+      ) : (
+        <UserCircleIcon height={size} width={size} />
+      )}
+      {onEdit ? (
+        <PencilSquareIcon
+          className="absolute top-[12%] right-[12%] bg-white p-[3%] rounded-full"
+          height={size / 4}
+          onClick={onEdit}
+          width={size / 4}
+        />
+      ) : null}
+    </div>
   );
 }
--- apps/frontend/src/components/Avatar/types.ts
+++ apps/frontend/src/components/Avatar/types.ts
@@ -1,4 +1,5 @@
 export interface AvatarProps {
   imageUri?: string;
   size: number;
+  onEdit?: () => void;
 }
