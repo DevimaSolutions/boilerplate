521d72420b713e3ce3b54e4c44f25f7a4ae875f6
Viktoria Monastyrska <viktoria.m@devima.solutions>
2024-01-08 15:36:58 +0200

    fix: conditions, return statement

--- apps/frontend/src/utils/recaptcha/verify-recaptcha.ts
+++ apps/frontend/src/utils/recaptcha/verify-recaptcha.ts
@@ -11,10 +11,10 @@ export async function verifyRecaptcha(action: string) {
   await ensureGoogleRecaptchaLoaded();
 
   if (!env.reCaptcha.siteKey) {
-    return Promise.resolve({ hasValidRecaptchaScore: true });
+    return { score: 1, hasValidRecaptchaScore: true };
   }
 
-  return new Promise<{ hasValidRecaptchaScore: boolean }>((res, rej) => {
+  return new Promise<{ score: number; hasValidRecaptchaScore: boolean }>((res, rej) => {
     window.grecaptcha.ready(async () => {
       try {
         const token = await window.grecaptcha.execute(env.reCaptcha.siteKey, { action });
@@ -22,7 +22,7 @@ export async function verifyRecaptcha(action: string) {
         if (response.action !== action) {
           throw new Error('Incorrect action');
         }
-        res({ hasValidRecaptchaScore: response.score >= 5 });
+        res({ score: response.score, hasValidRecaptchaScore: response.score > 0.5 });
       } catch (error) {
         if (error instanceof Error) {
           toast.error(error.message);
